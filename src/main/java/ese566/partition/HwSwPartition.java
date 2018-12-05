98a5bbebad1d5bc2eff5c5b3abe90d29b13fb4ae
Jerry <Jerry@Boru.Li>
2018-12-04 23:54:26 -0500

    skeleton of code

--- src/main/java/ese566/partition/HwSwPartition.java
+++ src/main/java/ese566/partition/HwSwPartition.java
@@ -2,4 +2,58 @@ package ese566.partition;
 
 public class HwSwPartition {
 
+	int [] [] adjacencyMatrix;
+	int [] [] degreeMatrix;
+	Node [] nodes;
+	
+	public HwSwPartition(int numNodes)
+	{
+		nodes = new Node[numNodes];
+	}
+	
+	public void outputParetoFront()
+	{
+		
+	}
+	
+	public void partition() throws Exception
+	{
+		
+		initPop();
+		
+		discreteDiffusion();
+		
+		while(!terminate())
+		{
+			implementationSelection();
+			discreteDiffusion();
+		}
+		
+	}
+	
+	private void initPop()
+	{
+		
+	}
+	
+	private void discreteDiffusion()
+	{
+		
+	}
+	
+	private boolean terminate()
+	{
+		boolean isSatisfied = false;
+		
+		return isSatisfied;
+	}
+	
+	private void implementationSelection()
+	{
+		
+	}
+	
+	
+	
+	
 }
