bf034c056206bc031a9c5adbd9516c85a11e42fb
Jerry <Jerry@Boru.Li>
2018-12-04 23:33:06 -0500

    implemented Task class

--- src/main/java/ese566/partition/Node.java
+++ src/main/java/ese566/partition/Node.java
@@ -1,5 +1,14 @@
 package ese566.partition;
 
+import java.util.ArrayList;
+
 public class Node {
 
+	private ArrayList<Task> tasks;
+	
+	public Node()
+	{
+		tasks = new ArrayList<Task>();
+	}
+	
 }
--- src/main/java/ese566/partition/Task.java
+++ src/main/java/ese566/partition/Task.java
@@ -4,12 +4,33 @@ public class Task {
 
 	private int weightSW;
 	private int weightHW;
-	boolean isSW;
+	private boolean isSW;
 	
 	public Task(int weightSW, int weightHW)
 	{
 		this.weightHW = weightHW;
 		this.weightSW = weightSW;
-		
+		isSW = false;
 	}
+	
+	public void setIsSW(boolean isSW)
+	{
+		this.isSW = isSW;
+	}
+	
+	public int getWeightSW()
+	{
+		return weightSW;
+	}
+	
+	public int getWeightHW()
+	{
+		return weightHW;
+	}
+	
+	public boolean isSW()
+	{
+		return isSW;
+	}
+	
 }
